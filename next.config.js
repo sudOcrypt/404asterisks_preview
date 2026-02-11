90ae11d9e8e77b57eb2e1f2487653a11b904f5ea
jared <jared@DESKTOP-20911B9>
2026-02-10 22:22:06 -0500

    Fix GitHub Pages: add basePath for subpath deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/404asterisks_preview',
+  assetPrefix: '/404asterisks_preview',
 }
 
 module.exports = nextConfig
